8c85ce262d5084dd7f459a1a77bd76ef39fab4df
Diego Da'Silva <smartcrashomg@gmail.com>
2022-05-11 09:20:20 -0400

    Init Phaser and create empty Scene

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import './style.css'
+import Phaser, { Game } from 'phaser'
+import { World } from './scenes/World'
 
-const app = document.querySelector<HTMLDivElement>('#app')!
-
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+new Game({
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  render: { pixelArt: true },
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false,
+      gravity: { y: 0 },
+    },
+  },
+  scene: [World],
+})
---
+++ src/scenes/World.ts
@@ -0,0 +1,5 @@
+import { Scene } from "phaser";
+
+export class World extends Scene {
+
+}
