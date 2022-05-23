0571d0bb034d7444ba27509fdaddfcf216a8ebf8
Diego Da'Silva <smartcrashomg@gmail.com>
2022-05-23 13:17:24 -0400

    Update HealthBar.ts

--- src/HealthBar.ts
+++ src/HealthBar.ts
@@ -1,6 +1,15 @@
 import { times } from 'lodash-es'
 
-const SPACING = 10
+enum Colors {
+  white = 0xffffff,
+  black = 0x000000
+}
+
+const defaultStyles = {
+  borderWidth: 1,
+  padding: 1,
+  spacing: 1,
+} as const
 
 export class HealthBar {
   private maxValue: number
@@ -12,28 +21,33 @@ export class HealthBar {
     private y: number,
     private width: number,
     private height: number,
-    { defaultValue, maxValue }: { defaultValue: number; maxValue: number }
+    { defaultValue, maxValue }: { defaultValue: number; maxValue: number },
+    { borderWidth, padding, spacing } = defaultStyles
   ) {
     this.maxValue = maxValue
 
+    //
+    // Render
+    //
     this.scene.add
-      .rectangle(this.x + this.width / 2, this.y, this.width, this.height, 0x000000)
-      .setStrokeStyle(SPACING / 2, 0xffffff)
+      .rectangle(this.x + this.width / 2, this.y, this.width, this.height, Colors.black)
+      .setStrokeStyle(borderWidth, Colors.white)
 
-    const cellWidth = (this.width - SPACING * 2 - SPACING * (this.maxValue - 1)) / this.maxValue
-    const cellHeight = this.height - SPACING * 2
+    const cellWidth = (this.width - (padding * 2) - borderWidth - spacing * (this.maxValue - 1)) / this.maxValue
+    const cellHeight = this.height - borderWidth - (padding * 2)
 
     this.hearths = times(maxValue, (index) => {
-      const rectX = this.x + cellWidth / 2 + SPACING + (SPACING + cellWidth) * index
+      const rectX = this.x + cellWidth / 2 + padding + (spacing + cellWidth) * index
       const rectY = this.y
 
-      return this.scene.add.rectangle(rectX, rectY, cellWidth, cellHeight, 0xffffff)
+      return this.scene.add.rectangle(rectX, rectY, cellWidth, cellHeight, Colors.white)
     })
 
+    // Set initial value
     this.setValue(defaultValue)
   }
 
-  setValue(value: number): void {
+  public setValue(value: number): void {
     this.hearths.forEach((rect, index) => {
       if (index < value) rect.setAlpha(1)
       else rect.setAlpha(0)
