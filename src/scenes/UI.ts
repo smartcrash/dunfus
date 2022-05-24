e0c47b7f79c35e7a853b812a50539f3f1f1db022
Diego Da'Silva <smartcrashomg@gmail.com>
2022-05-24 10:17:22 -0400

    Remove UI

--- src/scenes/UI.ts
+++ src/scenes/UI.ts
@@ -1,22 +1,7 @@
 import { Scene } from 'phaser';
-import { HealthBar } from '../HealthBar';
-import { Unit } from '../Unit';
 
 export class UI extends Scene {
-  private player!: Unit
 
-  constructor() {
-    super('ui')
-  }
+  constructor() { super('ui') }
 
-  init(data: { player: Unit }) {
-    this.player = data.player
-  }
-
-  create(): void {
-    const { hp, maxHp } = this.player.stats
-    const healthBar = new HealthBar(this, 100, 100, 300, 40, { defaultValue: hp, maxValue: maxHp })
-
-    this.player.on(Unit.Events.HIT, (value: number) => healthBar.setValue(value))
-  }
 }
